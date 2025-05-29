a959a05b7c09c902cbce96684380d1187bfdc808
Tarcisio Ads <tarcisioasd@gmail.com>
2025-05-29 08:13:19 -0300

    feat: enable position monitor and process termination handlers

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,21 @@ const tradeCronJob = new TradeCronJob();
 tradeCronJob.execute();
 
 // Start position monitor cron job
-//const positionMonitorCron = new PositionMonitorCronJob();
-//positionMonitorCron.start().catch((error: Error) => {
-//    console.error('Error starting PositionMonitorCronJob:', error);
-//    process.exit(1);
-//});
+const positionMonitorCron = new PositionMonitorCronJob();
+positionMonitorCron.start().catch((error: Error) => {
+    console.error('Error starting PositionMonitorCronJob:', error);
+    process.exit(1);
+});
 
 // Handle process termination
-//process.on('SIGINT', () => {
-//    console.log('\nGracefully shutting down...');
-//    positionMonitorCron.stop();
-//    process.exit(0);
-//});
+process.on('SIGINT', () => {
+    console.log('\nGracefully shutting down...');
+    positionMonitorCron.stop();
+    process.exit(0);
+});
 
-//process.on('SIGTERM', () => {
-//    console.log('\nGracefully shutting down...');
-//    positionMonitorCron.stop();
-//    process.exit(0);
-//}); 
+process.on('SIGTERM', () => {
+    console.log('\nGracefully shutting down...');
+    positionMonitorCron.stop();
+    process.exit(0);
+}); 
